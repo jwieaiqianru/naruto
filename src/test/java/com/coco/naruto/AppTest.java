5fb931f81a49700b6c6002f21010f6a592ef4be5
jiangwei <jiangwei@hualala.com>
2021-02-05 14:30:32 +0800

    first commit

---
+++ src/test/java/com/coco/naruto/AppTest.java
@@ -0,0 +1,20 @@
+package com.coco.naruto;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+}
